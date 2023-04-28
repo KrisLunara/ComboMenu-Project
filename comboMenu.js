da2f8c9e8f6189b175981b7cd45b65aec3d73de7
9ut4 <9ut4@fresnou.org>
2023-04-28 15:25:52 -0700

    commit 04-28-2023 | Added iteration 2 & 3 and fixed iteration 1

--- comboMenu.js
+++ comboMenu.js
@@ -1,14 +1,13 @@
 const readLine = require('readline-sync');
 
 //To Do:
-//Make Drinks Function
-// Make different size options
+//Drinks
 // Ask for Ice maybe????
 //
-//Make Fries Function
-// Size options
+//Make Fries
+//Ask for ketchup if fries are selected
+//
 // Upgrading Order option
-// Maybe have different toppings or fry toppings idk
 //
 //Make Working Tax Cal
 //
@@ -24,7 +23,7 @@ let SandwichType = readLine.question(`Please select from one of three types of s
 Sandwich Number: `);
 
 //Got the thing to do the thing when it isn't a valid option :)
-while (SandwichType !== '1' ){
+while (SandwichType !== '1' && SandwichType !== '2' && SandwichType !== '3'){
     console.log('!ERROR!');
     SandwichType = readLine.question(`Please select from one of three types of sandwiches:\n
 1. Chicken $5.25 | 2. Beef $6.25 | 3. Tofu $5.75\n
@@ -42,14 +41,106 @@ if (SandwichType === '1'){
     console.log('Tofu');
     Order.push('Tofu');
     Cost.push(5.75);
+}else{
+    console.log('This is a line to indicate an error has occurred.' +
+        '\nPlease restart your order and try again.\nIf you continue seeing this message, please contact Molly' +
+        'or Mallory from tech support.');
+}
+
+console.log(Order);
+console.log(Cost);
+
+//Drink
+let Drink = readLine.question('Would you like a drink?\n(Y/N): ');
+while(Drink !=='Y' && Drink !== 'y' && Drink !== 'N' && Drink !== 'n'){
+    console.log('Please select a valid option.');
+    Drink = readLine.question('Would you like a drink?\n(Y/N): ');
+}
+if (Drink === 'Y' || Drink === "y" ){
+    let DrinkSize = readLine.question('Please select a drink size' +
+        '\n1. Small $1.00 | 2. Medium $1.50 | 3. Large $2.00 | 4. Cancel\nSize Number: ');
+
+    if (DrinkSize === '1'){
+        console.log('Small Drink');
+        Order.push('Small Drink');
+        Cost.push(1.00);
+    }else if(DrinkSize === '2'){
+        console.log('Medium Drink');
+        Order.push('Medium Drink');
+        Cost.push(1.50);
+    }else if(DrinkSize === '3'){
+        console.log('Large Drink');
+        Order.push('Large Drink');
+        Cost.push(2.00);
+    }else if(DrinkSize === '4'){
+        console.log('No Drink');
+        Order.push('No Drink');
+        Cost.push(0.00);
+    }else if(DrinkSize !=='1' && DrinkSize !== '2' && DrinkSize !== '3 '&& DrinkSize !== '4'){
+        while(DrinkSize !=='1' && DrinkSize !== '2' && DrinkSize !== '3 '&& DrinkSize !== '4'){
+            console.log('Please select a size.');
+            Drink = readLine.question('1. Small $1.00 | 2. Medium $1.50 | 3. Large $2.00 | 4. Cancel\nSize Number: ');
+        }
+    }else{
+        console.log('This is a line to indicate an error has occurred.' +
+            '\nPlease restart your order and try again.\nIf you continue seeing this message, please contact Molly' +
+            'or Mallory from tech support.');
+    }
+}
+if(Drink === 'N' || Drink === 'n'){
+    console.log('No Drink');
+    Order.push('No Drink');
+    Cost.push(0.00);
 }
 
 console.log(Order);
 console.log(Cost)
 
-//Drink
-
 //Fries
+let Fry = readLine.question('Would you like fries?\n(Y/N): ');
+while(Fry !=='Y' && Drink !== 'y' && Drink !== 'N' && Drink !== 'n'){
+    console.log('Please select a valid option.');
+    Drink = readLine.question('Would you like fries?\n(Y/N): ');
+}
+if (Fry === 'Y' || Fry === "y" ){
+    let FrySize = readLine.question('Please select a fry size' +
+        '\n1. Small $1.00 | 2. Medium $1.50 | 3. Large $2.00 | 4. Cancel\nSize Number: ');
+
+    if (FrySize === '1'){
+        console.log('Small Fries');
+        Order.push('Small Fries');
+        Cost.push(1.00);
+    }else if(FrySize === '2'){
+        console.log('Medium Fries');
+        Order.push('Medium Fries');
+        Cost.push(1.50);
+    }else if(FrySize === '3'){
+        console.log('Large Fries');
+        Order.push('Large Fries');
+        Cost.push(2.00);
+    }else if(FrySize === '4'){
+        console.log('No fries');
+        Order.push('No Fries');
+        Cost.push(0.00);
+    }else if(FrySize !=='1' && Drink !== '2' && FrySize !== '3 '&& FrySize !== '4'){
+        while(FrySize !=='1' && FrySize !== '2' && FrySize !== '3 '&& FrySize !== '4'){
+            console.log('Please select a size.');
+            Drink = readLine.question('1. Small $1.00 | 2. Medium $1.50 | 3. Large $2.00 | 4. Cancel\nSize Number: ');
+        }
+    }else{
+        console.log('This is a line to indicate an error has occurred.' +
+            '\nPlease restart your order and try again.\nIf you continue seeing this message, please contact Molly' +
+            'or Mallory from tech support.');
+    }
+}
+if(Fry === 'N' || Fry === 'n'){
+    console.log('No Drink');
+    Order.push('No Drink');
+    Cost.push(0.00);
+}
+
+console.log(Order);
+console.log(Cost)
 
 //Tax Season
 
@@ -64,3 +155,11 @@ console.log(Cost)
 // ║  your receipt to take to  ║\n
 // ║ counter! Have a good day! ║\n
 // ╚═══════════════════════════╝`); //Order Complete
+
+
+//Credits and Help
+
+//Created by Kris Martinez (https://github.com/KrisLunara)
+//Do not use or modify without permission
+//
+//This program has been made with the help of Molly from the Rubber Duck Tech Support Dept.
